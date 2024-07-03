ea64e258e667b7b97baf2f13cb38604fbec95494
harsh <harshjaiswal9450@gmail.com>
2024-07-03 21:18:12 +0530

    Secrets removed

---
+++ Constant.mjs
@@ -0,0 +1,21 @@
+export var New_Connection = "New_Connection";
+export var New_Message = "New_Message";
+export var Collection_name = "users";
+export var WS_SENDER_ID = "sender_id";
+export var WS_SEND_TO_ID = "sendto_id";
+export var MESSAGES = "messages";
+export var WS_MESSAGE = "message";
+export var WS_TYPE = "type";
+export var FIRESTORE_REGISTRATION_TOKEN = "registrationToken";  
+export var FCM_TOKEN = "token";
+export var FCM_SENDER_ID = "sender_id";
+export var FCM_SEND_TO_ID = "sendto_id";
+export var WS_GROUP_ID = "group_id";
+export var FCM_TITLE = "title";
+export var FCM_MESSAGE = "message";
+export var WS_NEW_GROUP_MESSAGE = "New_Group_Message";
+export var Collection_Group = "groups";
+export var Collection_Channel = "channels";
+export var Document_Group_Members = "members";
+export var serviceAccountKey ={
+  }
